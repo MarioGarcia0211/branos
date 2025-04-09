06f1083c255dd77c50e45b13ccf8349de5e1d74a
MarioGarcia0211 <jhonmariogarciaballesteros@gmail.com>
2025-04-09 15:29:53 -0500

    tercero commit mapa de procesos

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import { useState } from 'react'
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import MapaProcesos from './components/MapaProcesos';
-import Prueba from './components/Prueba';
 
 function App() {
   const [count, setCount] = useState(0)
@@ -10,7 +9,6 @@ function App() {
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<MapaProcesos/>}/>
-        <Route path="/prueba" element={<Prueba/>}/>
       </Routes>
     </BrowserRouter>
   )
--- src/components/MapaProcesos.jsx
+++ src/components/MapaProcesos.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
-import '../styles/Prueba.css'
+import '../styles/mapaProcesos.css'
 
 const MapaProcesos = () => {
     const navigate = useNavigate();
